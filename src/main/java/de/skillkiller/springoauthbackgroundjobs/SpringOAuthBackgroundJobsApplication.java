803aed23e4c124109b2bda0f1cb082a537e35de4
Skillkiller <seblsebastian@aol.de>
2024-10-24 23:49:34 +0200

    Init project

---
+++ src/main/java/de/skillkiller/springoauthbackgroundjobs/SpringOAuthBackgroundJobsApplication.java
@@ -0,0 +1,13 @@
+package de.skillkiller.springoauthbackgroundjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOAuthBackgroundJobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOAuthBackgroundJobsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/skillkiller/springoauthbackgroundjobs/SpringOAuthBackgroundJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.skillkiller.springoauthbackgroundjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOAuthBackgroundJobsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
